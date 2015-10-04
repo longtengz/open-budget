38af82a1c41aba473a10e824bf31c0c654836e94
hatswa <hatswa@hotmail.com>
2015-10-03 22:01:32 -0400

    html
    10:01

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-var express = require("express");
-
+var express = require('express');
 var app = express();
 
 app.set('view engine', 'jade');
-
-app.get('/', function(req, res) {
-    res.render('template/index', {title: 'New Brunswick Open Budget', message: 'Hello there!'});
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Home', message: 'Hello there!'});          //
 });
+app.use(express.static(__dirname + '/public'));                           //
+app.use(express.static(__dirname + '/public'));
 
+var server = app.listen(3000, "127.0.0.1", function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-var server = app.listen(8080, "10.240.0.3", function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
